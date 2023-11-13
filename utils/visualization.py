88c6230ea85a577ba4209d732eb79c807f6b2e75
Alex <118281736+alexmrin@users.noreply.github.com>
2023-11-13 17:20:39 +0900

    typo

--- utils/visualization.py
+++ utils/visualization.py
@@ -6,7 +6,7 @@ import numpy as np
 import vars as v
 
 def visualize(pred, label):
-    pred, label = pred.cpu().detatch().view(28 ,28).numpy(), label.cpu().detach().view(28, 28).numpy()
+    pred, label = pred.cpu().detach().view(28 ,28).numpy(), label.cpu().detach().view(28, 28).numpy()
 
     fig, ax = plt.subplots(1, 2, figsize=(16, 8))
     ax[0].imshow(pred)
